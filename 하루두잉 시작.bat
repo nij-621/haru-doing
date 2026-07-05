ec69fb9ac3c3f07c42552cc6eddf0160aacab383
nij-621 <jmoj5827@gmail.com>
2026-07-05 22:00:18 +0200

    Delete 하루두잉 시작.bat

--- 하루두잉 시작.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem 하루두잉 로컬 서버 실행 + 브라우저 열기
-start "haru-doing-server" powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0serve.ps1"
-timeout /t 2 >nul
-start "" http://localhost:8321/
